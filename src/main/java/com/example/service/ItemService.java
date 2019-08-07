9fade85f4382ca3c7a86a5dd34323437f8b43a3e
Lehotsky <lukas.lehotsky@accenture.com>
2019-08-07 10:01:25 +0200

    ref

--- src/main/java/com/example/service/ItemService.java
+++ src/main/java/com/example/service/ItemService.java
@@ -12,6 +12,9 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.data.domain.Page;
@@ -23,6 +26,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ItemService extends Utils {
+
+    private static final Logger logger = LoggerFactory.getLogger(ItemService.class);
 
     @Autowired
     private ItemRepository itemRepository;
@@ -59,8 +64,11 @@ public class ItemService extends Utils {
     	
     	Page<DBItem> items = itemRepository.findAll(paging);
     	List<ItemResponse> itemResponses = new ArrayList<>();
-    	
+
+        logger.info("!!!!!!!!!!!!!!!!!! ITEMS - "+ items.getContent() +"!!!!!!!!!!!!!!!!!!!!!!!!");
+
     	for(DBItem item: items.getContent()) {
+            logger.info("!!!!!!!!!!!!!!!!!! ITEMS - "+ items.getSize() +"!!!!!!!!!!!!!!!!!!!!!!!!");
     		itemResponses.add(generateItemResponse(item));
     	}
     	return itemResponses;
